d6e3d09ef1ede8fbd28b51a14c4a43914ca7dae3
Phi-star <saliuphilip8@gmail.com>
2025-03-17 16:26:40 +0100

    Create dashboard.js

---
+++ pubic/js/dashboard.js
@@ -0,0 +1,8 @@
+function applyForLoan() {
+    window.location.href = "/loan.html";
+}
+
+function logout() {
+    alert("You have been logged out.");
+    window.location.href = "/index.html"; // Redirect to login page
+}
